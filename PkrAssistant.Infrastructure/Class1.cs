3148cbbd7ccbd769ee61f1311eb98ab2086e3afe
akolodka <koldun008@gmail.com>
2026-02-18 22:21:06 +0300

    PkrAssistant migration to .NET initial commit
    - Added Domain, Application, Infrastructure, Api, Tests projects
    
    - Configured project references
    
    - Added appsettings template for PostgreSQL
    
    - Added global.json for SDK version

---
+++ PkrAssistant.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PkrAssistant.Infrastructure;
+
+public class Class1
+{
+
+}
---
+++ PkrAssistant.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PkrAssistant.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
