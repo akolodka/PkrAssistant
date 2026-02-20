9521f836c87d60104287951490b88dd3f0c96885
akolodka <koldun008@gmail.com>
2026-02-20 21:32:53 +0300

    feat: Добавлена сущность "Утверждённый тип средства измерений"

---
+++ PkrAssistant.Domain/ApprovedMeasuringInstrumentType.cs
@@ -0,0 +1,62 @@
+﻿namespace PkrAssistant.Domain
+{
+    /// <summary>
+    /// Утверждённый тип средства измерений
+    /// </summary>
+    public class ApprovedMeasuringInstrumentType
+    {
+        public Guid Id { get; private set; }
+
+        public string RegistrationNumber { get; private set; }
+
+        public int? VerificationIntervalYears { get; private set; }
+
+        // Внешние ключи
+        public Guid MeasuringInstrumentTypeId { get; private set; }
+        public Guid VerificationMethodId { get; private set; }
+
+        // Навигационные свойства
+        public MeasuringInstrumentType Type { get; private set; }
+        public VerificationMethod Method { get; private set; }
+
+        public ICollection<MeasuringInstrumentModification> Modifications { get; private set; }
+        public ICollection<MeasuringInstrument> Instruments { get; private set; }
+
+        // Для EF
+        public ApprovedMeasuringInstrumentType() {}
+
+        public ApprovedMeasuringInstrumentType(
+            string registrationNumber, 
+            Guid measuringInstrumentTypeId,
+            Guid verificationMethodId,
+            int? verificationIntervalYears = null)
+        {
+            if (string.IsNullOrWhiteSpace(registrationNumber) == true)
+            {
+                throw new ArgumentException("Регистрационный номер не может быть пустым", nameof(registrationNumber));
+            }
+
+            if (measuringInstrumentTypeId == Guid.Empty)
+            {
+                throw new ArgumentException("Тип средства измерений должен быть указан", nameof(measuringInstrumentTypeId));
+            }
+
+            if (verificationMethodId == Guid.Empty)
+            {
+                throw new ArgumentException("Методика поверки должна быть указана", nameof(verificationMethodId)); 
+            }
+
+            Id = Guid.NewGuid();
+            RegistrationNumber = registrationNumber;
+
+            VerificationIntervalYears = verificationIntervalYears;
+            MeasuringInstrumentTypeId = measuringInstrumentTypeId;
+
+            VerificationMethodId = verificationMethodId;
+
+            Modifications = new List<MeasuringInstrumentModification>();
+            Instruments = new List<MeasuringInstrument>();
+        }
+
+    }
+}
