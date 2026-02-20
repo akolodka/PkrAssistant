a847951aa82ca9060ab529bd068a6784cc04e830
akolodka <koldun008@gmail.com>
2026-02-20 22:04:09 +0300

    Добавлена сущность "Модификация типа средства измерений"

---
+++ PkrAssistant.Domain/MeasuringInstrumentModification.cs
@@ -0,0 +1,45 @@
+﻿namespace PkrAssistant.Domain
+{
+    /// <summary>
+    /// Модификация типа средства измерений
+    /// </summary>
+    public class MeasuringInstrumentModification
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int? VerificationIntervalYears { get; private set; }
+
+        // Внешний ключ
+        public Guid ApprovedMeasuringInstrumentTypeId { get; private set; }
+
+        // Навигационное свойство
+        public ApprovedMeasuringInstrumentType ApprovedType { get; private set; }
+
+        // для EF
+        public MeasuringInstrumentModification() {}
+
+        public MeasuringInstrumentModification(
+            string name,
+            Guid approvedMeasuringInstrumentTypeId,
+            int? verificationIntervalYears)
+        {
+            if (string.IsNullOrWhiteSpace(name) == true)
+            {
+                throw new ArgumentException("Наименование модификации должно быть указано", nameof(name));
+            }
+
+            if (approvedMeasuringInstrumentTypeId == Guid.Empty) 
+            { 
+                throw new ArgumentException("Тип утверждённого средства измерений должен быть указан", nameof(approvedMeasuringInstrumentTypeId));
+            }
+
+            Id = Guid.NewGuid();
+            Name = name.Trim();
+
+            VerificationIntervalYears = verificationIntervalYears;
+            ApprovedMeasuringInstrumentTypeId = approvedMeasuringInstrumentTypeId;
+        }
+    }
+}
