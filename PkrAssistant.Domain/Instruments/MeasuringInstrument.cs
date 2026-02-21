e7a27c0ad875346487c54dd2d52c3f50d1d27177
akolodka <koldun008@gmail.com>
2026-02-21 12:50:23 +0300

    Обновлены пространства имён

---
+++ PkrAssistant.Domain/Instruments/MeasuringInstrument.cs
@@ -0,0 +1,42 @@
+﻿namespace PkrAssistant.Domain.Instruments;
+
+/// <summary>
+/// Конкретное средство измерений
+/// </summary>
+public class MeasuringInstrument
+{
+    public Guid Id { get; private set; }
+    public string SerialNumber { get; private set; }
+    public int? VerificationIntervalYears { get; private set; }
+
+    // Внешний ключ
+    public Guid MeasuringInstrumentModificationId { get; private set; }
+
+    // Навигационное свойство
+    public MeasuringInstrumentModification Modification { get; private set; }
+
+    // Для EF
+    private MeasuringInstrument() {}
+
+    public MeasuringInstrument(
+        string serialNumber, 
+        Guid measuringInstrumentModificationId,
+        int? verificationIntervalYears = null)
+    {
+        if (string.IsNullOrWhiteSpace(serialNumber))
+        {
+            throw new ArgumentException("Заводской номер не может быть пустым", nameof(serialNumber));
+        }
+
+        if (measuringInstrumentModificationId == Guid.Empty)
+        {
+            throw new ArgumentException("Модификация должна быть указана", nameof(measuringInstrumentModificationId));
+        }
+
+        Id = Guid.NewGuid();
+        SerialNumber = serialNumber.Trim();
+        
+        VerificationIntervalYears = verificationIntervalYears;
+        MeasuringInstrumentModificationId = measuringInstrumentModificationId;
+    }
+}
---
+++ PkrAssistant.Domain/Instruments/MeasuringInstrumentModification.cs
@@ -0,0 +1,42 @@
+﻿namespace PkrAssistant.Domain.Instruments;
+
+/// <summary>
+/// Модификация типа средства измерений
+/// </summary>
+public class MeasuringInstrumentModification
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int? VerificationIntervalYears { get; private set; }
+
+    // Внешний ключ
+    public Guid ApprovedMeasuringInstrumentTypeId { get; private set; }
+
+    // Навигационное свойство
+    public ApprovedMeasuringInstrumentType ApprovedType { get; private set; }
+
+    // для EF
+    private MeasuringInstrumentModification() {}
+
+    public MeasuringInstrumentModification(
+        string name,
+        Guid approvedMeasuringInstrumentTypeId,
+        int? verificationIntervalYears = null)
+    {
+        if (string.IsNullOrWhiteSpace(name) == true)
+        {
+            throw new ArgumentException("Наименование модификации не может быть пустым", nameof(name));
+        }
+
+        if (approvedMeasuringInstrumentTypeId == Guid.Empty) 
+        { 
+            throw new ArgumentException("Тип утверждённого средства измерений должен быть указан", nameof(approvedMeasuringInstrumentTypeId));
+        }
+
+        Id = Guid.NewGuid();
+        Name = name.Trim();
+
+        VerificationIntervalYears = verificationIntervalYears;
+        ApprovedMeasuringInstrumentTypeId = approvedMeasuringInstrumentTypeId;
+    }
+}
