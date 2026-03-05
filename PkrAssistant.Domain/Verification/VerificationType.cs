0755b7114d2ac26fee01e5c0b48cc398f598e506
akolodka <koldun008@gmail.com>
2026-03-05 22:27:59 +0300

    feat: Добавлено перечисление типа поверки.

---
+++ PkrAssistant.Domain/Verification/VerificationType.cs
@@ -0,0 +1,17 @@
+﻿namespace PkrAssistant.Domain.Verification;
+
+/// <summary>
+/// Тип поверки.
+/// </summary>
+public enum VerificationType
+{ 
+    /// <summary>
+    /// Первичная поверка.
+    /// </summary>
+    Primary = 0,
+
+    /// <summary>
+    /// Периодическая поверка.
+    /// </summary>
+    Periodic = 1
+}
