dda0c28258e087dbde161e0e65d1e70988b91262
akolodka <koldun008@gmail.com>
2026-03-09 22:05:38 +0300

    feat: Добавлена сущность "Единица измерений", удалена сущность "Методика поверки".

---
+++ PkrAssistant.Domain/Standards/UnitOfMeasurement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Standards;
+
+/// <summary>
+/// Единица измерений физической величины.
+/// </summary>
+public class UnitOfMeasurement
+{
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Наименование единицы измерений.
+    /// </summary>
+    public string Name { get; private set; }
+
+    // Для EF
+    private UnitOfMeasurement() { }
+
+    public UnitOfMeasurement(string name)
+    {
+        if(string.IsNullOrWhiteSpace(name) == true)
+        {
+            throw new ArgumentException("Наименование единицы величины не может быть пустым", nameof(name));
+        }
+
+        Id = Guid.NewGuid();
+        Name = name.Trim();
+    }
+}
