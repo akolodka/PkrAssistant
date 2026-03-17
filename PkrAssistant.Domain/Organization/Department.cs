b3efa77ecbabb98ee359a489c0836c64b64f2d5d
akolodka <koldun008@gmail.com>
2026-03-17 21:57:30 +0300

    feat: Добавлена сущность Подразделение организации

---
+++ PkrAssistant.Domain/Organization/Department.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Organization;
+
+/// <summary>
+/// Подразделение организации, выполняющее поверку.
+/// </summary>
+public class Department
+{
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Наименование подразделения (не более 50 символов).
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// Комментарий к записи (не более 100 символов).
+    /// </summary>
+    public string Comment { get; private set; }
+
+    // Для EF
+    private Department() {}
+
+    public Department(
+        string name, 
+        string comment)
+    {
+        if (string.IsNullOrWhiteSpace(name) == true)
+        {
+            throw new ArgumentException("Наименование подразделения не может быть пустым", nameof(name));
+        }
+
+        if (string.IsNullOrWhiteSpace(comment) == true)
+        {
+            throw new ArgumentException("Комментарий не может быть пустым", nameof(comment));
+        }
+
+        Id = Guid.NewGuid();
+
+        Name = name.Trim();
+        Comment = comment.Trim();
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
