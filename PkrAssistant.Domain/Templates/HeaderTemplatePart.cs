596b96a3026faa9cc3de90ada13d0ced8a00f26a
akolodka <koldun008@gmail.com>
2026-03-18 21:51:02 +0300

    feat: Добавлена сущность "Заголовочная часть шаблона поверки".

---
+++ PkrAssistant.Domain/Templates/HeaderTemplatePart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Templates;
+
+/// <summary>
+/// Заголовочная часть шаблона поверки.
+/// </summary>
+public class HeaderTemplatePart : TemplatePart
+{
+    /// <summary>
+    /// Идентификатор подразделения, применяющего шаблон.
+    /// </summary>
+    public Guid DepartmentId { get; private set; }
+
+    // Для EF
+    private HeaderTemplatePart() {}
+
+    public HeaderTemplatePart(
+        string fileName,
+        byte[] fileContent,
+        Guid departmentId)
+        : base(TemplatePartType.Header, fileName, fileContent)
+    {
+        if (departmentId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор подразделения должен быть заполнен", nameof(departmentId));
+        }
+
+        DepartmentId = departmentId;
+    }
+
+    // Для отладки
+    public override string ToString()
+    {
+        return $"{FileName} ({DepartmentId})";
+    }
+}
