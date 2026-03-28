d8c8cad43a770e034be7472cce2f6b803c59e4ff
akolodka <koldun008@gmail.com>
2026-03-28 13:09:32 +0300

    feat: Добавлена сущность "Ранг эталона",

---
+++ PkrAssistant.Domain/Standards/StandardRank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Standards;
+
+/// <summary>
+/// Разряд эталона.
+/// </summary>
+public class StandardRank
+{
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Наименование разряда эталона.
+    /// </summary>
+    public string Name { get; private set; }
+
+    // Для EF
+    private StandardRank() {}
+
+    public StandardRank(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name) == true)
+        {
+            throw new ArgumentException("Наименование разряда эталона не может быть пустым", nameof(name));
+        }
+
+        Id = Guid.NewGuid();
+        Name = name.Trim();
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
