aed0ec81c693bec4085745cae48682a7c1c047d3
akolodka <koldun008@gmail.com>
2026-03-28 13:10:47 +0300

    feat: Добавлена сущность "Комбинация единицы измерений и разряда эталона".

---
+++ PkrAssistant.Domain/Standards/UnitRankCombination.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Standards;
+
+/// <summary>
+/// Комбинация единица измерений - разряд эталона.
+/// </summary>
+public class UnitRankCombination
+{
+    public Guid Id { get; private set; }
+    
+    /// <summary>
+    /// Идентификатор единицы измерений.
+    /// </summary>
+    public Guid UnitOfMeasurementId { get; private set; }
+
+    /// <summary>
+    /// Идентификатор ранга эталона.
+    /// </summary>
+    public Guid StandardRankId { get; private set; }
+
+    // Для EF
+    private UnitRankCombination() {}
+
+    public UnitRankCombination(Guid unitOfMeasurementId, Guid standardRankId)
+    {
+        if (unitOfMeasurementId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор единицы измерений должен быть указан", nameof(unitOfMeasurementId));
+        }
+
+        if (standardRankId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор ранга эталона должен быть указан", nameof(standardRankId));
+        }
+
+        Id = Guid.NewGuid();
+
+        UnitOfMeasurementId = unitOfMeasurementId;
+        StandardRankId = standardRankId;
+    }
+
+    public override string ToString()
+    {
+        return $"Combination {Id}";
+    }
+}
