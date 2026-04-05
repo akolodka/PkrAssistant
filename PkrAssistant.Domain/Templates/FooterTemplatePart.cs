3280ea4c7dcfa20cdc65710f53ce0da64525c6ff
akolodka <koldun008@gmail.com>
2026-04-05 16:52:17 +0300

    feat: Добавлена сущность части шаблона поверки, содержащая заключение по результатам поверки и подпись.

---
+++ PkrAssistant.Domain/Templates/FooterTemplatePart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Templates;
+
+/// <summary>
+/// Часть шаблона поверки, содержащая заключение по результатам поверки и подпись.
+/// </summary>
+public class FooterTemplatePart : TemplatePart
+{
+    /// <summary>
+    /// Идентификатор подразделения шаблона (для гарантии сохранности ширины шаблона).
+    /// </summary>
+    public Guid DepartmentId { get; private set; }
+
+    // Для EF
+    private FooterTemplatePart() {}
+
+    public FooterTemplatePart(
+        Guid departmentId,
+        string fileName,
+        byte[] fileContent)
+        : base(TemplatePartType.Footer, fileName, fileContent)
+    {
+        if (departmentId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор подразделения должен быть указан", nameof(departmentId));
+        }
+
+        DepartmentId = departmentId;
+    }
+
+    // Для отладки
+    public override string ToString()
+    {
+        return $"{FileName} (Department = {DepartmentId}, Type = {TemplatePartType.Footer})";
+    }
+}
