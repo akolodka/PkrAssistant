9c041a9801803e8eca26920b5ad76af0f8d778ae
akolodka <koldun008@gmail.com>
2026-04-05 16:24:28 +0300

    feat: Добавлена сущность части шаблона -- блок внешнего осмотра и опробования.

---
+++ PkrAssistant.Domain/Templates/PreliminaryInspectionPart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Templates;
+
+/// <summary>
+/// Часть шаблона поверки, содержащая блок внешнего осмотра и опробования.
+/// </summary>
+public class PreliminaryInspectionPart : TemplatePart
+{
+    /// <summary>
+    /// Идентификатор подразделения шаблона (для гарантии сохранности ширины шаблона).
+    /// </summary>
+    public Guid DepartmentId { get; private set; }
+
+    // Для EF
+    private PreliminaryInspectionPart() {}
+
+    public PreliminaryInspectionPart(
+        Guid departmentId, 
+        string fileName,
+        byte[] fileContent)
+        : base(TemplatePartType.PreliminaryInspection, fileName, fileContent)
+    {
+        if (departmentId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор подразделения должен быть указан", nameof(departmentId));
+        }
+
+        DepartmentId = departmentId;
+    }
+
+    // Для отладки
+    public override string ToString()
+    {
+        return $"{FileName} (Department = {DepartmentId}, Type = {TemplatePartType.PreliminaryInspection})";
+    }
+}
