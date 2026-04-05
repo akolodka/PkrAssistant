038ad68527842dd34b6577451e0e165c2d392810
akolodka <koldun008@gmail.com>
2026-04-05 16:46:46 +0300

    feat: Добавлена сущность части шаблона поверки, содержащая блок определения конкретной метрологической характеристики.

---
+++ PkrAssistant.Domain/Templates/MetrologicalInspectionPart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PkrAssistant.Domain.Templates;
+
+/// <summary>
+/// Часть шаблона поверки, содержащая блок определения конкретной метрологической характеристики.
+/// </summary>
+public class MetrologicalInspectionPart : TemplatePart
+{
+    /// <summary>
+    /// Идентификатор подразделения шаблона (для гарантии сохранности ширины шаблона).
+    /// </summary>
+    public Guid DepartmentId { get; private set; }
+
+    // Для EF
+    private MetrologicalInspectionPart() {}
+
+    public MetrologicalInspectionPart(
+        Guid departmentId, 
+        string fileName, 
+        byte[] fileContent)
+        : base(TemplatePartType.MetrologicalInspection, fileName, fileContent)
+    {
+        if (departmentId == Guid.Empty)
+        {
+            throw new ArgumentException("Идентификатор подразделения должен быть указан", nameof(departmentId));
+        }
+
+        DepartmentId = departmentId;
+    }
+
+    // Для отладки
+    public override string ToString()
+    {
+        return $"{FileName} (Department = {DepartmentId}, Type = {TemplatePartType.MetrologicalInspection})";
+    }
+}
