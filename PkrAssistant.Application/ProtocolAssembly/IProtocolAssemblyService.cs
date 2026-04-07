078af59fb467657f51f2890dd1f49cd594879cf4
akolodka <koldun008@gmail.com>
2026-04-07 22:14:40 +0300

    feat: Добавлен контракт IProtocolAssemblyService

---
+++ PkrAssistant.Application/ProtocolAssembly/IProtocolAssemblyService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace PkrAssistant.Application.ProtocolAssembly;
+
+/// <summary>
+/// Контракт сборки документа-шаблона поверки.
+/// </summary>
+public interface IProtocolAssemblyService
+{
+    /// <summary>
+    /// Собирает документ из частей.
+    /// </summary>
+    Task<ProtocolAssemblyResult> AssembleAsync(AssemblyRequest request);
+}
