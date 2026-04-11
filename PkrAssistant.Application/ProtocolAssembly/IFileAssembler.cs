664da64d8f59a5b2fbf65e72881212638891ed88
akolodka <koldun008@gmail.com>
2026-04-11 12:36:25 +0300

    feat: Добавлен контракт IFileAssembler

---
+++ PkrAssistant.Application/ProtocolAssembly/IFileAssembler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PkrAssistant.Application.ProtocolAssembly;
+
+/// <summary>
+/// Контракт сборки шаблона из составных частей.
+/// </summary>
+public interface IFileAssembler
+{
+    /// <summary>
+    /// Собирает шаблон поверки из составных частей.
+    /// </summary>
+    /// <param name="templatePartContents">Содержимое частей шаблонов поверки в порядке сборки.</param>
+    /// <returns>Файл шаблона поверки.</returns>
+    Task<byte[]> AssembleAsync(IEnumerable<byte[]> templatePartContents);
+}
