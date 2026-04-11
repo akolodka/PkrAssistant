6c213444e0353c0a973277cfd06f62c9385f5ceb
akolodka <koldun008@gmail.com>
2026-04-11 10:48:17 +0300

    feat: Добавлен контракт ITemplatePartProvider

---
+++ PkrAssistant.Application/ProtocolAssembly/ITemplatePartProvider.cs
@@ -0,0 +1,19 @@
+﻿using PkrAssistant.Domain.Templates;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PkrAssistant.Application.ProtocolAssembly;
+
+/// <summary>
+/// Контракт на получение частей шаблона поверки из хранилища данных.
+/// </summary>
+public interface ITemplatePartProvider
+{
+    /// <summary>
+    /// Загружает части шаблона по их идентификаторам.
+    /// </summary>
+    /// <param name="templatePartIds">Список идентификаторов частей шаблона поверки.</param>
+    /// <returns>Список частей шаблона поверки.</returns>
+    Task<IReadOnlyList<TemplatePart>> GetPartsByIdsAsync(IReadOnlyList<Guid> templatePartIds);
+}
