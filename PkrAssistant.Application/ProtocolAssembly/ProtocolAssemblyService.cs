530b202e7ff5855160647e815db9cc3fbbc3f690
akolodka <koldun008@gmail.com>
2026-04-15 22:03:29 +0300

    feat: Добавлен каркас ProtocolAssemblyService

---
+++ PkrAssistant.Application/ProtocolAssembly/ProtocolAssemblyService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace PkrAssistant.Application.ProtocolAssembly;
+
+/// <summary>
+/// Собирает шаблон поверки.
+/// </summary>
+public class ProtocolAssemblyService : IProtocolAssemblyService
+{
+    private readonly ITemplatePartProvider _provider;
+    private readonly IFileAssembler _assembler;
+
+    public ProtocolAssemblyService(
+        ITemplatePartProvider provider, 
+        IFileAssembler assembler)
+    {
+        _provider = provider;
+        _assembler = assembler;
+    }
+
+    public Task<ProtocolAssemblyResult> AssembleAsync(AssemblyRequest request)
+    {
+        return Task.FromResult(ProtocolAssemblyResult.Failure("Not implemented"));
+    }
+}
