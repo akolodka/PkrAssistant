4a59c29ceb61a35aaaef3032fad3377aaea14056
akolodka <koldun008@gmail.com>
2026-04-19 09:35:27 +0300

    feat: Добавлен FakeFileAssembler для тестирования сборки частей шаблона в памяти.

---
+++ PkrAssistant.Infrastructure/FakeFileAssembler.cs
@@ -0,0 +1,40 @@
+﻿using PkrAssistant.Application.ProtocolAssembly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PkrAssistant.Infrastructure;
+
+/// <summary>
+/// Заглушка для тестирования сборки частей шаблоа.
+/// </summary>
+public class FakeFileAssembler : IFileAssembler
+{
+    /// <summary>
+    /// Склеивает части шаблона в один файл в памяти.
+    /// </summary>
+    /// <param name="templatePartContents">Список байтовых массивов частей шаблона в порядке склеивания.</param>
+    /// <returns>Объединённый массив байтов шаблона.</returns>
+    public Task<byte[]> AssembleAsync(IEnumerable<byte[]> templatePartContents)
+    {
+        if (templatePartContents == null)
+        {
+            throw new ArgumentNullException("Список байтовых массивов частей шаблона не может быть null", nameof(templatePartContents));
+        }
+
+        using var stream = new MemoryStream();
+
+        foreach (byte[] content in templatePartContents)
+        {
+            if(content == null)
+            {
+                throw new ArgumentNullException("Содержимое части шаблона не может быть null", nameof(content));
+            }
+
+            stream.Write(content);
+        }
+
+        return Task.FromResult(stream.ToArray());
+    }
+}
