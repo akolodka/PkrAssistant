1aaa77feb4008df53b0d3ee5c290eff58a4a583e
akolodka <koldun008@gmail.com>
2026-04-22 22:26:41 +0300

    test: Добавлен сценарий с негативным тестом при отсутствии частей шаблонов

--- PkrAssistant.Tests/ProtocolAssemblyServiceTests.cs
+++ PkrAssistant.Tests/ProtocolAssemblyServiceTests.cs
@@ -65,4 +65,51 @@ public class ProtocolAssemblyServiceTests
             new byte[] { 1, 2, 3, 4, }, 
             result.FileContent);
     }
+
+    [Fact]
+    public async Task AssembleAsync_WithoutAllParts_ReturnsFailure()
+    {
+        var provider = new InMemoryTemplatePartProvider();
+
+        var header = new HeaderTemplatePart(
+            Guid.NewGuid(),
+            "Шапка шаблона",
+            new byte[] {1,2});
+
+        provider.AddPart(header);
+
+        var neck = new NeckTemplatePart(
+            Guid.NewGuid(),
+            header.Id,
+            "Список эталонов шаблона",
+            new byte[] { 3, 4 });
+
+        var assembler = new FakeFileAssembler();
+
+        var service = new ProtocolAssemblyService(provider, assembler);
+
+        IReadOnlyList<Guid> parts = new List<TemplatePart>()
+        {
+            header,
+            neck
+        }
+        .Select(p => p.Id)
+        .ToArray();
+
+        var request = new AssemblyRequest(Guid.NewGuid(), parts);
+
+        //Act
+        var result = await service.AssembleAsync(request);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+
+        Assert.NotNull(result.ErrorMessage);
+
+        Assert.Contains(
+            neck.Id.ToString(), 
+            result.ErrorMessage);
+
+        Assert.Null(result.FileContent);
+    }
 }
