89d18c3a5f58698a9171ea13fdda2f010c696922
akolodka <koldun008@gmail.com>
2026-05-11 13:42:48 +0300

    feat: ClosedXmlFileAssmebler - добавлен "боевой" сервис сборки файла Excel из частей; - добавлены базовые юнит-тесты проверки работы сервиса сборки файла Excel

---
+++ PkrAssistant.Infrastructure/ClosedXmlFileAssembler.cs
@@ -0,0 +1,66 @@
+﻿using ClosedXML.Excel;
+using PkrAssistant.Application.ProtocolAssembly;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PkrAssistant.Infrastructure;
+
+/// <summary>
+/// «Боевой» сервис сборки частей шаблона. 
+/// </summary>
+public class ClosedXmlFileAssembler : IFileAssembler
+{
+    /// <summary>
+    /// Склеивает части шаблона в один файл в памяти.
+    /// </summary>
+    /// <param name="templatePartContents">Список байтовых массивов частей шаблона в порядке склеивания.</param>
+    /// <returns>Объединённый массив байтов шаблона.</returns>
+    public async Task<byte[]> AssembleAsync(IEnumerable<byte[]> templatePartContents) 
+    {
+        using var stream = new MemoryStream();
+        using var destination = new XLWorkbook();
+
+        var worksheet = destination.AddWorksheet();
+
+        foreach (var content in templatePartContents)
+        {
+            ProcessAssemble(worksheet, content);
+        }
+
+        destination.SaveAs(stream);
+
+        var fileContent = stream.ToArray();
+
+        return await Task.FromResult(fileContent);
+    }
+
+    /// <summary>
+    /// Добавляет содержимое части шаблона в конец целевого листа.
+    /// </summary>
+    /// <param name="worksheet">Лист, который выполняется вставка данных.</param>
+    /// <param name="fileContent">Содержимое, подлежащее вставке.</param>
+    private void ProcessAssemble(IXLWorksheet worksheet, byte[] fileContent)
+    {
+        using var stream = new MemoryStream(fileContent);
+        using var book = new XLWorkbook(stream);
+
+        var source = book.Worksheet(1)
+            .RangeUsed(XLCellsUsedOptions.AllContents);
+        
+        if (source == null)
+        {
+            return;
+        }
+
+        var lastUsedCell = worksheet.LastCellUsed(XLCellsUsedOptions.AllContents);
+
+        var destination = (lastUsedCell == null)
+            ? worksheet.Cell("A1")
+            : lastUsedCell.WorksheetRow()
+                .RowBelow()
+                .FirstCell();
+
+        source.CopyTo(destination);
+    }
+}
---
+++ PkrAssistant.Tests/ClosedXmlFileAssemblerTests.cs
@@ -0,0 +1,57 @@
+﻿using ClosedXML.Excel;
+using PkrAssistant.Infrastructure;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PkrAssistant.Tests;
+
+public class ClosedXmlFileAssemblerTests
+{
+    [Fact]
+    public async Task AssembleAsync_ReturnsFileContent()
+    {
+        // Arrange
+        var assembler = new ClosedXmlFileAssembler();
+
+        // Act
+        var fileContent = await assembler.AssembleAsync(Array.Empty<byte[]>());
+
+        // Assert
+        Assert.True(fileContent.Length > 0);
+    }
+
+    [Fact]
+    public async Task AssembleAsync_WithStubContent_ReturnsCombinedContent()
+    {
+        // Arrange
+        using var book = new XLWorkbook();
+        var sheet = book.AddWorksheet();
+
+        var textValue = "Write Test";
+
+        sheet.Cell("A1").Value = textValue;
+
+        using var stream = new MemoryStream();
+        book.SaveAs(stream);
+
+        var templateParts = stream.ToArray();
+        
+        var assembler = new ClosedXmlFileAssembler();
+
+        // Act
+        var fileContent = await assembler.AssembleAsync([templateParts]);
+
+        using var resultStream = new MemoryStream(fileContent);
+        using var resultBook = new XLWorkbook(resultStream);
+
+        var resultValue = resultBook.Worksheet(1).Cell("A1").Value;
+
+        // Assert
+        Assert.True(fileContent.Length > 0);
+        Assert.Equal(textValue, resultValue);
+        
+        // Для визуальной проверки
+        File.WriteAllBytes(@"C:\Users\akolodka\Desktop\test.xlsx", fileContent);
+    }
+}
