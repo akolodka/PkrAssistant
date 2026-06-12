c37220d06d8d80f3cdfaeafb4989146bd16c2326
akolodka <koldun008@gmail.com>
2026-06-12 10:38:56 +0300

    feat: Добавлена сущность VerifierEntity с слое Infrastructure

---
+++ src/PkrAssistant.Infrastructure/Data/Entities/VerifierEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PkrAssistant.Infrastructure.Data.Entities;
+
+internal class VerifierEntity
+{
+    public Guid Id { get; set; }
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string? Patronymic { get; set; }
+
+    public string Position {  get; set; } = string.Empty;
+}
